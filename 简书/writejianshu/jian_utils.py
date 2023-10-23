39d30f7de05f95ae7498401322b9792114e6256a
mxz <836323493@qq.com>
2023-10-23 14:35:44 +0800

    'title:'

--- 简书/writejianshu/jian_utils.py
+++ 简书/writejianshu/jian_utils.py
@@ -12,6 +12,13 @@ import re
 
 import requests
 
+proxy = '127.0.0.1:7890'
+
+proxies = {
+    'http': 'http://' + proxy,
+    'https': 'https://' + proxy
+}
+
 folder_path = "../note_o/2023"
 IMG_DIR = "../img"
 directory_path = "../note_o"
@@ -247,7 +254,7 @@ def get_content(id:str):
     response = requests.get('https://www.jianshu.com/author/notes/{}/content'.format(id), cookies=cookies, headers=headers)
     return response.json()["content"]
 def notice_wechat(title: str):
-    response = requests.get('https://sctapi.ftqq.com/SCT142512TIZeFu7Dj22drBfQgwT0KPIdI.send?title={}'.format(title))
+    response = requests.get('https://sctapi.ftqq.com/SCT142512TIZeFu7Dj22drBfQgwT0KPIdI.send?title={}'.format(title), proxies=proxies)
     return response.json()
 
 def note_list():
@@ -298,4 +305,5 @@ if __name__ == '__main__':
         notice_wechat(filename)
     except Exception as e:
         print(e)
+        notice_wechat("str(e)")
 
