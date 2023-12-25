2cc17475515b83978c65653d2c47d4e2afe57518
mxz <836323493@qq.com>
2023-12-25 17:45:46 +0800

    小鱼

--- src/pages/api/chat/index.ts
+++ src/pages/api/chat/index.ts
@@ -1,8 +1,32 @@
 import type { APIRoute } from "astro";
+import { escapeHtml } from "@pages/chat/escape.js";
+
+const defaultData = { chatList: [] };
+const setCache = (key, data) => env.MXZ_BACK.put(key, data);
+const getCache = key => env.MXZ_BACK.get(key);
 
 export const GET: APIRoute = async ({ request, redirect }) => {
   try {
-    return new Response("{'name':22}", {
+    const cacheKey = `data`;
+    let data;
+    const cache = await getCache(cacheKey);
+    if (!cache) {
+      await setCache(cacheKey, JSON.stringify(defaultData));
+      data = defaultData;
+    } else {
+      data = JSON.parse(cache);
+    }
+
+    const list = JSON.stringify(
+      data.chatList?.map(todo => ({
+        id: escapeHtml(todo.id),
+        name: escapeHtml(todo.name),
+        message: escapeHtml(todo.message),
+        date: escapeHtml(todo.date),
+      })) ?? []
+    );
+    return new Response(list, {
+      status: 200,
       headers: { "Content-Type": "application/json; charset=UTF-8" },
     });
   } catch (error) {
@@ -10,5 +34,17 @@ export const GET: APIRoute = async ({ request, redirect }) => {
       status: 500,
     });
   }
-  return redirect("/dashboard");
+};
+export const PUT: APIRoute = async ({ request, redirect }) => {
+  try {
+    const body = await request.text();
+    const cacheKey = `data`;
+    JSON.parse(body);
+    var data = await setCache(cacheKey, body);
+    return new Response(body, { status: 200 });
+  } catch (error) {
+    return new Response("Something went wrong", {
+      status: 500,
+    });
+  }
 };
