a4a58f596b02ab6c29eaeb68c7eac39d9541e906
mx <480471449@qq.com>
2025-11-22 11:35:56 +0800

    优化页面

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,19 @@ import { SITE } from "./src/config";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import markdoc from "@astrojs/markdoc";
-import {livePhotoPlugin} from "./src/utils/livePhotoPlugin.ts";
-import {bilibiliPlugin} from "./src/utils/bilibiliPlugin.ts";
+import { livePhotoPlugin } from "./src/utils/livePhotoPlugin.ts";
+import { bilibiliPlugin } from "./src/utils/bilibiliPlugin.ts";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [mdx(), sitemap(), tailwind(), react(), markdoc()],
-    markdown: {
+  markdown: {
     //     // 应用于 .md 和 .mdx 文件
-        remarkPlugins: [[remarkToc, {heading: "目录"}], livePhotoPlugin, bilibiliPlugin]
+    remarkPlugins: [[remarkToc, { heading: "目录" }], livePhotoPlugin, bilibiliPlugin]
     //     remarkPlugins: [[remarkToc, {heading: "目录"}], bilibiliPlugin]
-    },
+  },
+  image: {
+    domains: ["pub-4232cd0528364004a537285f400807bf.r2.dev"],
+  },
 });
