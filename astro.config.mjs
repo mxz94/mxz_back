4d763d5ae9712c0c04d8a5c5a63ba36a8674fd93
mx <480471449@qq.com>
2026-04-30 11:12:37 +0800

    优化页面布局与加载性能

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { SITE } from "./src/config";
-import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import markdoc from "@astrojs/markdoc";
 import { livePhotoPlugin } from "./src/utils/livePhotoPlugin.ts";
@@ -12,7 +11,7 @@ import { bilibiliPlugin } from "./src/utils/bilibiliPlugin.ts";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [mdx(), sitemap(), tailwind(), react(), markdoc()],
+  integrations: [mdx(), sitemap(), tailwind(), markdoc()],
   markdown: {
     //     // 应用于 .md 和 .mdx 文件
     remarkPlugins: [[remarkToc, { heading: "目录" }], livePhotoPlugin, bilibiliPlugin]
